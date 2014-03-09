f9f8cb3575a97ff730b63ac21e2d55bacc6b4ca0
Gabriel Puliatti <gabriel@adzuna.com>
2014-03-09 03:12:11 +0000

    Price API call works for doge

--- first.py
+++ first.py
@@ -15,42 +15,60 @@ import json
 import urllib2
 
 
-class RateCall:
-    
+class PriceCall:
     def __init__(self, params):
         self.currencypair = params['currencypair']
-        if self.currencypair = 'dogebtc':
+        if self.currencypair == 'dogebtc':
             self.prices = self.doge2btc
     
-    def prices:
+    def prices(self):
         return self.prices
             
-    def doge2btc(self):
-        cryptsydata = json.load(urllib2.urlopen("http://pubapi.cryptsy.com/api.php?method=marketdatav2"))['return']
-        self.prices['dogebtc'] = cryptsydata['markets']['DOGE/BTC']['lasttradeprice']
+    def dogebtc(self):
+        try:
+            for i in range(0,5):
+                while True:
+                    try:
+                        data = json.load(urllib2.urlopen("http://pubapi.cryptsy.com/api.php?method=marketdatav2"))['return']
+                        self.prices['dogebtc'] = data['markets']['DOGE/BTC']['lasttradeprice'] # cryptsy hardcoded for now
+                        return self.prices
+                    except:
+                        continue
+                    break
+        except:
+            # log error, price failed after 5 tries
+            raise IOError
+
+        
         #self.prices['dogeltc'] = cryptsydata['markets']['DOGE/LTC']['lasttradeprice']
-        return self.prices
+        
 
 class Checker:
-    
     def __init__(self,params):
         self.startttime = datetime.now
         self.payments = params['transactions']
+        self.received = []
+        self.not_received = []
     
-    def check:
+    def check(self):
         for transaction in self.payments:
             payment_info = transaction.payment_info
             
-            if payment_info['source_currencty'] = 'btc':
+            if payment_info['currencypair'][3:] == 'btc':
                 try:
                     self.check_btc(payment_info)
                 except:
-                    print "Check failed"
-                    raise
-    def check_btc
-                
+                    print "Check failed" #log
+                    self.not_received.append(transaction)
+                    
+    def check_btc(self, payment_info):
+        # check it is btc        
+        if payment_info['currencypair'][3:] = 'btc':
+            print payment_info['currencypair'][3:]
+            # check bitcoin blockchain
+                 
     
-class PaymentWorker:
+class Payer:
     
     def __init__(self, params):
         self.starttime = datetime.now
@@ -81,42 +99,35 @@ class Transaction:
         self.currencypair = params['currency_pair']
         self.flipdirection = False #params['direction']  
         self.sourceaddress = params['source_address'] # client address
-        self.receivingaddress = params['receiving_address'] # where we're picking it up from, should be same coin as sourceaddress
+        self.receivingaddress = params['receiving_address'] # where we're picking it up from, should be same altcoin as sourceaddress
         self.destaddress = params['destination_address']
         self.email = params['email']
         #self.emailout = params['email_out']
         self.createtime = datetime.now
-        self.prices = self.get_prices
+        self.price = self.get_price
         if self.flipdirection = False:
-            self.amountout = self.amountin * self.prices[self.currencypair] # sending amount
+            self.amountout = self.amountin * self.price[self.currencypair] # sending amount
         else:
-            self.amountout = self.amountin / self.prices[self.currencypair] # sending amount
-            
+            self.amountout = self.amountin / self.price[self.currencypair] # sending amount        
     
-    def receive_info:
-        receive_info = {}
-        receive_info['amount_in'] = self.amountin
-        receive_info['source_address'] = self.sourceaddress
-        receive_info['receivingaddress'] self.receivingaddress
-        receive_info['email'] = self.email
-        
-    
-    def payment_info:
+    def payment_info(self):
         payment_info = {}
         payment_info['amount_out'] = self.amountout
         payment_info['currencypair'] = self.currencypair
+        payment_info['receiving_address'] = self.receivingaddress
         payment_info['source'] = self.sourceaddress
         payment_info['destination'] = self.destaddress
         payment_info['email'] = self.email
         if self.flipdirection = False:
             payment_info['dest_currency'] = payment_info['currencypair'][:3]
+            payment_info['source_currency'] = payment_info['currencypair'][3:]
         else:
             payment_info['dest_currency'] = payment_info['currencypair'][3:]
-        
+            payment_info['source_currency'] = payment_info['currencypair'][:3]
         #payment_info['email_out'] = self.emailout
         return payment_info
                 
-    def get_prices(self):
-        # run scrapy
-        return RateCall({'currencypair':self.currencypair}).prices
+    def get_price(self):
+        callparams = {'currencypair':self.currencypair}
+        return PriceCall(callparams).prices
        
---
+++ price_call.py
@@ -0,0 +1,38 @@
+import json
+import urllib2
+
+class PriceCall:
+    def __init__(self, params):
+        self.currencypair = params['currencypair']
+        if self.currencypair == 'dogebtc':
+            self.price = self.dogebtc()
+    
+    def __str__(self):
+        return str(self.price)
+    
+    def price(self):
+        return self.price
+    
+    def dogebtc(self):
+        return self.get_data('DOGE/BTC')
+        
+                    
+    def get_data(self, pair):
+        price = int
+        set = False
+        while bool(set) == False:
+            try:
+                data = json.load(urllib2.urlopen("http://pubapi.cryptsy.com/api.php?method=marketdatav2"))['return']
+                price = data['markets'][pair]['lasttradeprice']
+                set = True
+                break
+            except:
+                print str(Exception)
+                continue
+        return price
+
+
+price = PriceCall({'currencypair':'dogebtc'})
+
+
+print price
